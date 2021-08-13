7b5b706ad036cff005645feef9c5743af5217cb8
Francisco  Carlos <franciscocarlos@MacBook-Pro-de-Francisco.local>
2021-08-13 00:37:22 -0300

    feat: db configuration

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: 'arena200',
+  database: 'techchallange',
+  define: {
+    timestamps: true,
+    underscored: true,
+  }
+};
